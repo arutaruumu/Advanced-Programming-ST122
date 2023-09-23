1dc3387c3a1e72ccb41af6ed7ef6a64702012453
Arta RM <77877876+arutaruumu@users.noreply.github.com>
2023-09-23 18:03:36 +0700

    Add files via upload

---
+++ oop_pemrog/oop_pemrog/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+abstract class Kendaraan
+{
+    public string Nama { get; set; }
+
+    public Kendaraan(string nama)
+    {
+        Nama = nama;
+    }
+
+    public abstract void Bergerak();
+}
+
+class Mobil : Kendaraan
+{
+    public Mobil(string nama) : base(nama) { }
+
+    public override void Bergerak()
+    {
+        Console.WriteLine(Nama + " bergerak dengan roda empat.");
+    }
+}
+
+class Sepeda : Kendaraan
+{
+    public Sepeda(string nama) : base(nama) { }
+
+    public override void Bergerak()
+    {
+        Console.WriteLine(Nama + " bergerak dengan menggunakan tenaga kaki.");
+    }
+}
+
+class Pesawat : Kendaraan
+{
+    public Pesawat(string nama) : base(nama) { }
+
+    public override void Bergerak()
+    {
+        Console.WriteLine(Nama + " terbang di udara.");
+    }
+}
+
+class Garasi
+{
+    public void Parkir(Kendaraan kendaraan)
+    {
+        Console.WriteLine("Memarkirkan " + kendaraan.Nama);
+        kendaraan.Bergerak();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Mobil sedan = new Mobil("Sedan");
+        Sepeda sepeda = new Sepeda("Polygon");
+        Pesawat pesawat = new Pesawat("Boeing 747");
+
+        Garasi garasi = new Garasi();
+
+        garasi.Parkir(sedan);
+        garasi.Parkir(sepeda);
+        garasi.Parkir(pesawat);
+    }
+}
