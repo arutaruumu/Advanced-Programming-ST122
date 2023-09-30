800915b8785c699da820f1bd787654775c47bf40
Arta RM <77877876+arutaruumu@users.noreply.github.com>
2023-09-30 22:19:35 +0700

    Add files via upload

---
+++ P4/AplikasiInputDataMahasiswa/AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    internal class Mahasiswa
+    {
+        public string Nim;
+        public string Nama;
+        public string Kelas;
+        public string Nilai_Huruf;
+        public int Nilai;
+    }
+}
