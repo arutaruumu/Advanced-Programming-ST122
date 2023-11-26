b45e20dc71b483cb448e4d57e072590f78c8cef6
Arta RM <77877876+arutaruumu@users.noreply.github.com>
2023-11-27 04:46:28 +0700

    Add files via upload

---
+++ P10/PerpustakaanAppMVC/PerpustakaanAppMVC/Model/Repository/MahasiswaRepository.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+using PerpustakaanAppMVC.Model.Entity;
+using PerpustakaanAppMVC.Model.Context;
+
+namespace PerpustakaanAppMVC.Model.Repository
+{
+    public class MahasiswaRepository
+    {
+        private SQLiteConnection _conn;
+
+        public MahasiswaRepository(DbContext context)
+        {
+            _conn = context.Conn;
+        }
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+            string sql = @"insert into mahasiswa (npm, nama, angkatan)
+values (@npm, @nama, @angkatan)";
+
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, _conn))
+            {
+                cmd.Parameters.AddWithValue("@npm", mhs.Npm);
+                cmd.Parameters.AddWithValue("@nama", mhs.Nama);
+                cmd.Parameters.AddWithValue("@angkatan", mhs.Angkatan);
+                try
+                {
+                    result = cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print("Create error: {0}", ex.Message);
+                }
+            }
+            return result;
+        }
+
+
+        //TUGAS 10.1
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+            string sql = @"update mahasiswa
+                   set nama = @nama, angkatan = @angkatan
+                   where npm = @npm";
+
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, _conn))
+            {
+                cmd.Parameters.AddWithValue("@npm", mhs.Npm);
+                cmd.Parameters.AddWithValue("@nama", mhs.Nama);
+                cmd.Parameters.AddWithValue("@angkatan", mhs.Angkatan);
+                try
+                {
+                    result = cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print("Update error: {0}", ex.Message);
+                }
+            }
+            return result;
+        }
+
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+            string sql = @"delete from mahasiswa where npm = @npm";
+
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, _conn))
+            {
+                cmd.Parameters.AddWithValue("@npm", mhs.Npm);
+                try
+                {
+                    result = cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print("Delete error: {0}", ex.Message);
+                }
+            }
+            return result;
+        }
+
+
+        public List<Mahasiswa> ReadAll()
+        {
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            try
+            {
+                string sql = @"select npm, nama, angkatan from mahasiswa order by nama";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, _conn))
+                {
+                    using (SQLiteDataReader dtr = cmd.ExecuteReader())
+                    {
+                        while (dtr.Read())
+                        {
+                            Mahasiswa mhs = new Mahasiswa();
+                            mhs.Npm = dtr["npm"].ToString();
+                            mhs.Nama = dtr["nama"].ToString();
+                            mhs.Angkatan = dtr["angkatan"].ToString();
+                            list.Add(mhs);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print("ReadAll error: {0}", ex.Message);
+            }
+            return list;
+        }
+
+
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            try
+            {
+                string sql = @"select npm, nama, angkatan from mahasiswa where nama like @nama order by nama";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, _conn))
+                {
+                    cmd.Parameters.AddWithValue("@nama", string.Format("%{0}%", nama));
+                    using (SQLiteDataReader dtr = cmd.ExecuteReader())
+                    {
+                        while (dtr.Read())
+                        {
+                            Mahasiswa mhs = new Mahasiswa();
+                            mhs.Npm = dtr["npm"].ToString();
+                            mhs.Nama = dtr["nama"].ToString();
+                            mhs.Angkatan = dtr["angkatan"].ToString();
+                            list.Add(mhs);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print("ReadByNama error: {0}", ex.Message);
+            }
+            return list;
+        }
+    }
+}
